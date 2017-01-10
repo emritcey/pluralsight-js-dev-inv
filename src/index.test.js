e68fc88bcb1079406cdb8be85e6a792427bf9f41
Emma Ritcey <Emma@user1s-MacBook-Pro.local>
2017-01-10 15:12:18 -0600

    learning

---
+++ src/index.test.js
@@ -0,0 +1,27 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+
+// Mocha
+describe('Our first tests', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+//JS DOM
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8")
+    jsdom.env(index, function(err, window) {
+      //window represents the window in the browser
+
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello, World!!!!");
+      done();
+      window.close();
+    });
+  })
+})
